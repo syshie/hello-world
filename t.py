74853d7c2e0ae461a0a869f25414e6d0c81c8ed4
syshie <e44168@gmail.com>
2019-08-24 15:19:22 +0000

    update t.py with version()

--- t.py
+++ t.py
@@ -1,1 +1,21 @@
-print('Hello world')
+# syshie
+
+import math
+import random
+import numpy as np
+import scipy as sp
+import sklearn as sk
+import pandas as pd
+import statsmodels as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def version():
+    mods = ['np', 'sp', 'sk', 'pd', 'sm', 'sns']
+    for mod in mods:
+        print(mod + ': ', eval(mod).__version__)
+
+
+if __name__ == '__main__':
+    version()
